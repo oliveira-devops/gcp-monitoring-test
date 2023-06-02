37e37a81fe34736f5b94fd3e50cec2b19fa99d40
Bruno Oliveira <bruno.santos@dadosfera.ai>
2023-06-02 08:27:37 -0300

    ajustes

--- tf-modules/disk_alert_policy/main.tf
+++ tf-modules/disk_alert_policy/main.tf
@@ -42,7 +42,7 @@ resource "google_monitoring_alert_policy" "alert_policy" {
       }
       aggregations {
         alignment_period   = "60s"
-        per_series_aligner = "ALIGN_SUM"
+        per_series_aligner = "ALIGN_NONE"
         cross_series_reducer = "REDUCE_SUM"       
       }
     }
